396cecae3e9e534ff3f559c82444273ec19a00de
giriftzen <enes.seveng37@gmail.com>
2022-11-09 16:04:38 +0300

    init

---
+++ src/test/java/com/example/enocatest/EnocaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enocatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
